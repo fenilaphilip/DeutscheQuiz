37e9246ea26c1f4316a30a6f37ef2c7fc267cba6
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-04-20 14:47:48 +0200

    Navbar items added.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 
-$(document).ready(function(){
-    
+$(document).ready(function () {
+    $.get("data/categories.json", function (navItems, status) {
+
+        console.log(navItems);
+        for (i = 0; i < navItems.length; i++) {
+            $("#categoryElement")
+                .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">"
+                    + navItems[i]
+                    + "</a></li>");
+        }
+    });
 });
