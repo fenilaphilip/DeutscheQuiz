2c7b2100014f094a91f29d42f08639164798c883
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-04-21 14:55:46 +0200

    Nav bar working prefectly.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
 
 $(document).ready(function () {
+    $("#about").show();
+    $("#nomen-class,#quiz-area,#submit-button").hide();
     $.get("data/categories.json", function (navItems, status) {
-
         console.log(navItems);
         for (i = 0; i < navItems.length; i++) {
             $("#categoryElement")
-                .append("<li class=\"nav-item\"><a class=\"nav-link\" href=\"#\">"
+                .append("<li class=\"nav-item\"><a class=\" cat-item nav-link \" href=\"#\">"
                     + navItems[i]
                     + "</a></li>");
         }
+        $(".cat-item").click(function () {
+            nav_link_clicked($(this).text())
+        });
     });
+
 });
+
+
+function nav_link_clicked(category) {
+    $("#nomen-class").html(category);
+    $("#about").hide();
+    $("#nomen-class,#quiz-area,#submit-button").show();
+}
+
+function about_clicked() {
+    $("#about").show();
+    $("#nomen-class,#quiz-area,#submit-button").hide();
+}
