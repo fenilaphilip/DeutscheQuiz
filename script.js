792bdcf0b7ccedb1da76b05d12cf30edc529ece1
Fenila Philip <100304937+fenilaphilip@users.noreply.github.com>
2022-04-22 13:19:44 +0200

    Displaying questions.

--- script.js
+++ script.js
@@ -14,17 +14,35 @@ $(document).ready(function () {
             nav_link_clicked($(this).text())
         });
     });
-
 });
 
-
 function nav_link_clicked(category) {
-    $("#nomen-class").html(category);
     $("#about").hide();
     $("#nomen-class,#quiz-area,#submit-button").show();
+    display_nomen(category);
 }
 
 function about_clicked() {
     $("#about").show();
     $("#nomen-class,#quiz-area,#submit-button").hide();
+}
+
+function display_nomen(category) {
+    $.get("data/" + category + ".json", function (object, status) {
+        let art = object.article;
+        $("#nomen-class").html(art + " " + category);
+        let len = object.words.length;
+        $("#quiz-area").html("");
+        for (j = 0; j < len; j++) {
+            $("#quiz-area").append(
+                '<li class="question">' +
+                '<input class="btn-check" type="radio" name="que-' + j + '" autocomplete="off" id="der' + j + '">' +
+                '<label class="btn btn-outline-success" for="der' + j + '"> Der </label> ' +
+                '<input class="btn-check" type="radio" name="que-' + j + '" autocomplete="off" id="die' + j + '">' +
+                '<label class="btn btn-outline-success" for="die' + j + '"> Die </label> ' +
+                '<input class="btn-check" type="radio"  name="que-' + j + '" autocomplete="off" id="das' + j + '">' +
+                '<label class="btn btn-outline-success" for="das' + j + '"> Das </label>' +
+                '<span id="nomen">' + object.words[j].nom + '</span></li>');
+        }
+    });
 }
